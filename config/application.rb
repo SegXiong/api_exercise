0bede3d8f76b12680d30c59e9cc39408d7db20a0
SegueX <free1989xj@hotmail.com>
2017-06-27 15:00:40 +0800

    ch2

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module ApiExercise
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    JUHE_CONFIG = Rails.application.config_for(:juhe)
   end
 end
