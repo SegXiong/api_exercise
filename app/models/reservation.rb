42f43afc7933a38da1d7f8e2f084fd2b034e12c4
SegueX <free1989xj@hotmail.com>
2017-06-30 16:51:35 +0800

    ch7

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,8 @@ class Reservation < ApplicationRecord
   belongs_to :train
   before_validation :generate_booking_code, :on => :create
 
+  belongs_to :user, :optional => true
+
   def generate_booking_code
     self.booking_code = SecureRandom.uuid
 
--- app/models/train.rb
+++ app/models/train.rb
@@ -2,8 +2,18 @@ class Train < ApplicationRecord
   validates_presence_of :number
   has_many :reservations
 
+  mount_uploader :train_logo, TrainLogoUploader
+
+  SEAT = begin
+    (1..6).to_a.map do |series|
+      ["A", "B", "C"].map do |letter|
+        "#{series}#{letter}"
+      end
+    end
+  end.flatten
+
   def available_seats
-    ["1A", "1B", "1C", "1D", "1F"]
+    return SEAT - self.reservations.pluck(:seat_number)
 
   end
 end
